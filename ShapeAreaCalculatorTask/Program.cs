abf32ebd1d365e7be0961478b95d98d87c6e6e2c
Al Stone <astone@heckgrammar.co.uk>
2024-09-26 09:37:01 +0100

    Add project files.

---
+++ ShapeAreaCalculatorTask/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ShapeAreaCalculatorTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write subroutines to calculate the areas of 
+            // Circle
+            // Triangle
+            // Rectangle
+            // Pentagon
+            // Octagon
+        }
+        static int areaOfRectangle()//Add the parameters
+        {
+
+        }
+    }
+}
